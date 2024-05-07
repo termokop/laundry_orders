f0822f62df6abb9dd2733d2e7967f893ca3a43bd
termokop <termokop564@gmail.com>
2024-05-06 23:30:05 -0600

    created RoomComponent

---
+++ src/components/RoomComponent.vue
@@ -0,0 +1,76 @@
+<script setup>
+
+import { ref } from 'vue';
+defineProps({
+  roomNumber: {
+    type: String,
+    required: true
+  }
+})
+
+let status = ref("None")
+    
+
+
+function changeStatus(value) {
+  status.value = value
+  console.log(status.value)
+}
+</script>
+
+<template>
+  <div class="room">
+    <div class="room-number">{{ roomNumber }}</div>
+    <div class="room-status">
+      <div
+        class="btn-status"
+        :class="status == 'None' ? 'choosen-btn-status' : ''"
+        @click="changeStatus('None')"
+      >
+        None
+      </div>
+      <div
+        class="btn-status"
+        :class="status == 'Stay Over' ? 'choosen-btn-status' : ''"
+        @click="changeStatus('Stay Over')"
+      >
+        Stay Over
+      </div>
+      <div
+        class="btn-status"
+        :class="status == 'Check Out' ? 'choosen-btn-status' : ''"
+        @click="changeStatus('Check Out')"
+      >
+        Check Out
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.room {
+    width: 100%;
+    background-color: aliceblue;
+    margin-bottom: 5px;
+}
+.room-number {
+    display: flex;
+    justify-content: center;
+    color: green;
+    font-size: x-large;
+}
+.room-status {
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #adffda;
+  cursor: pointer;
+}
+.btn-status {
+  width: 30%;
+}
+
+.choosen-btn-status {
+  background-color: lightgreen;
+}
+</style>
---
+++ src/components/linens_for_rooms.js
@@ -0,0 +1,75 @@
+const linens_for_checkOut = {
+    "CQQ": {
+        queen_pillow_case: 4,
+        queen_pillow_sham: 4,
+        queen_sheet: 4,
+        queen_duvet_cover: 2,
+        bath_towel: 2.8,
+        bath_matt: 2,
+        face_cloth: 2.8,
+        hand_towel: 2.3,
+        bath_robe: 1.9,
+    },
+    "CK": {
+        king_pillow_case: 2,
+        king_pillow_sham: 2,
+        king_sheet: 2,
+        king_duvet_cover: 1,
+        bath_towel: 1.5,
+        bath_matt: 1,
+        face_cloth: 2.2,
+        hand_towel: 1.8,
+        bath_robe: 1.2,
+    },
+    "CQ": {
+        queen_pillow_case: 2,
+        queen_pillow_sham: 2,
+        queen_sheet: 2,
+        queen_duvet_cover: 1,
+        bath_towel: 1.5,
+        bath_matt: 1,
+        face_cloth: 2.2,
+        hand_towel: 1.8,
+        bath_robe: 1.2,
+    },
+    
+}
+
+const linens_for_stayOver = {
+    "CQQ": {
+        queen_pillow_case: 0,
+        queen_pillow_sham: 0,
+        queen_sheet: 0,
+        queen_duvet_cover: 0,
+        bath_towel: 2.8,
+        bath_matt: 2,
+        face_cloth: 2.8,
+        hand_towel: 2.3,
+        bath_robe: 1.9,
+    },
+    "CK": {
+        king_pillow_case: 0,
+        king_pillow_sham: 0,
+        king_sheet: 0,
+        king_duvet_cover: 0,
+        bath_towel: 1.5,
+        bath_matt: 1,
+        face_cloth: 2.2,
+        hand_towel: 1.8,
+        bath_robe: 1.2,
+    },
+    "CQ": {
+        queen_pillow_case: 0,
+        queen_pillow_sham: 0,
+        queen_sheet: 0,
+        queen_duvet_cover: 0,
+        bath_towel: 1.5,
+        bath_matt: 1,
+        face_cloth: 2.2,
+        hand_towel: 1.8,
+        bath_robe: 1.2,
+    },
+    
+}
+
+export default {linens_for_checkOut, linens_for_stayOver}
