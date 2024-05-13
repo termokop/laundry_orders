c33ecfa4261c2471411188ac97013e9985ff0fb1
termokop <termokop564@gmail.com>
2024-05-12 21:44:47 -0600

    filled objects with data

--- src/components/linens_for_rooms.js
+++ src/components/linens_for_rooms.js
@@ -1,5 +1,5 @@
 export const linens_for_checkOut = {
-  CQQ: {
+  AQQ: {
     queen_pillow_case: 4,
     queen_pillow_sham: 4,
     queen_sheet: 4,
@@ -10,7 +10,29 @@ export const linens_for_checkOut = {
     hand_towel: 2.3,
     bath_robe: 1.9
   },
-  CK: {
+  SQQ: {
+    queen_pillow_case: 4,
+    queen_pillow_sham: 4,
+    queen_sheet: 4,
+    queen_duvet_cover: 2,
+    bath_towel: 2.8,
+    bath_matt: 2,
+    face_cloth: 2.8,
+    hand_towel: 2.3,
+    bath_robe: 1.9
+  },
+  SQ: {
+    queen_pillow_case: 2,
+    queen_pillow_sham: 2,
+    queen_sheet: 2,
+    queen_duvet_cover: 1,
+    bath_towel: 1.5,
+    bath_matt: 1,
+    face_cloth: 2.2,
+    hand_towel: 1.8,
+    bath_robe: 1.2
+  },
+  SK: {
     king_pillow_case: 2,
     king_pillow_sham: 2,
     king_sheet: 2,
@@ -21,11 +43,33 @@ export const linens_for_checkOut = {
     hand_towel: 1.8,
     bath_robe: 1.2
   },
-  CQ: {
-    queen_pillow_case: 2,
-    queen_pillow_sham: 2,
-    queen_sheet: 2,
-    queen_duvet_cover: 1,
+  DK: {
+    king_pillow_case: 2,
+    king_pillow_sham: 2,
+    king_sheet: 2,
+    king_duvet_cover: 1,
+    bath_towel: 1.5,
+    bath_matt: 1,
+    face_cloth: 2.2,
+    hand_towel: 1.8,
+    bath_robe: 1.2
+  },
+  LK: {
+    king_pillow_case: 2,
+    king_pillow_sham: 2,
+    king_sheet: 2,
+    king_duvet_cover: 1,
+    bath_towel: 1.5,
+    bath_matt: 1,
+    face_cloth: 2.2,
+    hand_towel: 1.8,
+    bath_robe: 1.2
+  },
+  SuiteK: {
+    king_pillow_case: 2,
+    king_pillow_sham: 2,
+    king_sheet: 2,
+    king_duvet_cover: 1,
     bath_towel: 1.5,
     bath_matt: 1,
     face_cloth: 2.2,
@@ -35,7 +79,7 @@ export const linens_for_checkOut = {
 }
 
 export const linens_for_stayOver = {
-  CQQ: {
+  AQQ: {
     queen_pillow_case: 0,
     queen_pillow_sham: 0,
     queen_sheet: 0,
@@ -46,7 +90,29 @@ export const linens_for_stayOver = {
     hand_towel: 2.3,
     bath_robe: 1.9
   },
-  CK: {
+  SQQ: {
+    queen_pillow_case: 0,
+    queen_pillow_sham: 0,
+    queen_sheet: 0,
+    queen_duvet_cover: 0,
+    bath_towel: 2.8,
+    bath_matt: 2,
+    face_cloth: 2.8,
+    hand_towel: 2.3,
+    bath_robe: 1.9
+  },
+  SQ: {
+    queen_pillow_case: 0,
+    queen_pillow_sham: 0,
+    queen_sheet: 0,
+    queen_duvet_cover: 0,
+    bath_towel: 1.5,
+    bath_matt: 1,
+    face_cloth: 2.2,
+    hand_towel: 1.8,
+    bath_robe: 1.2
+  },
+  SK: {
     king_pillow_case: 0,
     king_pillow_sham: 0,
     king_sheet: 0,
@@ -57,11 +123,33 @@ export const linens_for_stayOver = {
     hand_towel: 1.8,
     bath_robe: 1.2
   },
-  CQ: {
-    queen_pillow_case: 0,
-    queen_pillow_sham: 0,
-    queen_sheet: 0,
-    queen_duvet_cover: 0,
+  DK: {
+    king_pillow_case: 0,
+    king_pillow_sham: 0,
+    king_sheet: 0,
+    king_duvet_cover: 0,
+    bath_towel: 1.5,
+    bath_matt: 1,
+    face_cloth: 2.2,
+    hand_towel: 1.8,
+    bath_robe: 1.2
+  },
+  LK: {
+    king_pillow_case: 0,
+    king_pillow_sham: 0,
+    king_sheet: 0,
+    king_duvet_cover: 0,
+    bath_towel: 1.5,
+    bath_matt: 1,
+    face_cloth: 2.2,
+    hand_towel: 1.8,
+    bath_robe: 1.2
+  },
+  SuiteK: {
+    king_pillow_case: 0,
+    king_pillow_sham: 0,
+    king_sheet: 0,
+    king_duvet_cover: 0,
     bath_towel: 1.5,
     bath_matt: 1,
     face_cloth: 2.2,
--- src/components/rooms_info.js
+++ src/components/rooms_info.js
@@ -1,48 +1,736 @@
 export const rooms_info = [
-        {
-            room_number: "0302",
-            room_type: "CK",
-            status: "None"
-        },    {
-            room_number: "0303",
-            room_type: "CK",
-            status: "None"
-        },    {
-            room_number: "0304",
-            room_type: "CQ",
-            status: "None"
-        },    {
-            room_number: "0305",
-            room_type: "CQ",
-            status: "None"
-        },    {
-            room_number: "0306",
-            room_type: "CQ",
-            status: "None"
-        },    {
-            room_number: "0307",
-            room_type: "CQQ",
-            status: "None"
-        },    {
-            room_number: "0401",
-            room_type: "CQQ",
-            status: "None"
-        },    {
-            room_number: "0402",
-            room_type: "CK",
-            status: "None"
-        },    {
-            room_number: "0403",
-            room_type: "CQ",
-            status: "None"
-        },    {
-            room_number: "0501",
-            room_type: "CK",
-            status: "None"
-        },    {
-            room_number: "0502",
-            room_type: "CQ",
-            status: "None"
-        },
-       
-    ]
+  //////////////////////// FLOOR #3
+  {
+    room_number: '0303',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0304',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0305',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0306',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0307',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0308',
+    room_type: 'AQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0309',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0310',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0311',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0312',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0313',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0314',
+    room_type: 'SQ',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #4
+  {
+    room_number: '0401',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0402',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0403',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0404',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0405',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0406',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0407',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0408',
+    room_type: 'AQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0409',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0410',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0411',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0412',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0413',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0414',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0415',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #5
+  {
+    room_number: '0501',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0502',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0503',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0504',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0505',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0506',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0507',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0508',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0509',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0510',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0511',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0512',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0513',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0514',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0515',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #6
+  {
+    room_number: '0601',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0602',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0603',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0604',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0605',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0606',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0607',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0608',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0609',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0610',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0611',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0612',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0613',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0614',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0615',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #7
+  {
+    room_number: '0701',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0702',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0703',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0704',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0705',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0706',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0707',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0708',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0709',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0710',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0711',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0712',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0713',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0714',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0715',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #8
+  {
+    room_number: '0801',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0802',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0803',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0804',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0805',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0806',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0807',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0808',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0809',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0810',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0811',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0812',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0813',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0814',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0815',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #9
+  {
+    room_number: '0901',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0902',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0903',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0904',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0905',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0906',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0907',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0908',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '0909',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0910',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0911',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '0912',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '0913',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '0914',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '0915',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #10
+  {
+    room_number: '1001',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1002',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1003',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '1004',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1005',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1006',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '1007',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1008',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '1009',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1010',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '1011',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '1012',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1013',
+    room_type: 'LK',
+    status: 'None'
+  },
+  {
+    room_number: '1014',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '1015',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #11
+  {
+    room_number: '1101',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1102',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1103',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '1104',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1106',
+    room_type: 'SuiteK',
+    status: 'None'
+  },
+  {
+    room_number: '1107',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1108',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '1109',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1110',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '1111',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '1112',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1114',
+    room_type: 'SuiteK',
+    status: 'None'
+  },
+  {
+    room_number: '1115',
+    room_type: 'SK',
+    status: 'None'
+  },
+
+  ///////////// FLOOR #12
+  {
+    room_number: '1201',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1202',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1203',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '1204',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1206',
+    room_type: 'SuiteK',
+    status: 'None'
+  },
+  {
+    room_number: '1207',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1208',
+    room_type: 'SQQ',
+    status: 'None'
+  },
+  {
+    room_number: '1209',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1210',
+    room_type: 'SQ',
+    status: 'None'
+  },
+  {
+    room_number: '1211',
+    room_type: 'DK',
+    status: 'None'
+  },
+  {
+    room_number: '1212',
+    room_type: 'SK',
+    status: 'None'
+  },
+  {
+    room_number: '1214',
+    room_type: 'SuiteK',
+    status: 'None'
+  },
+  {
+    room_number: '1215',
+    room_type: 'SK',
+    status: 'None'
+  }
+]
