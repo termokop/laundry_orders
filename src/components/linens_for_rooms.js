d157a96fe28d8bc9bde7b73fa8c32532b87171bb
termokop <termokop564@gmail.com>
2024-05-15 18:08:33 -0600

    ajusted values for needed linens

--- src/components/linens_for_rooms.js
+++ src/components/linens_for_rooms.js
@@ -4,77 +4,77 @@ export const linens_for_checkOut = {
     queen_pillow_sham: 4,
     queen_sheet: 4,
     queen_duvet_cover: 2,
-    bath_towel: 2.8,
-    bath_matt: 2,
-    face_cloth: 2.8,
-    hand_towel: 2.3,
-    bath_robe: 1.9
+    bath_towel: 2,
+    bath_matt: 1,
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   SQQ: {
     queen_pillow_case: 4,
     queen_pillow_sham: 4,
     queen_sheet: 4,
     queen_duvet_cover: 2,
-    bath_towel: 2.8,
-    bath_matt: 2,
-    face_cloth: 2.8,
-    hand_towel: 2.3,
-    bath_robe: 1.9
+    bath_towel: 2,
+    bath_matt: 1,
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   SQ: {
     queen_pillow_case: 2,
     queen_pillow_sham: 2,
     queen_sheet: 2,
     queen_duvet_cover: 1,
-    bath_towel: 1.5,
+    bath_towel: 2,
     bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   SK: {
     king_pillow_case: 2,
     king_pillow_sham: 2,
     king_sheet: 2,
     king_duvet_cover: 1,
-    bath_towel: 1.5,
+    bath_towel: 2,
     bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   DK: {
     king_pillow_case: 2,
     king_pillow_sham: 2,
     king_sheet: 2,
     king_duvet_cover: 1,
-    bath_towel: 1.5,
-    bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    bath_towel: 4,
+    bath_matt: 2,
+    face_cloth: 3,
+    hand_towel: 3,
+    bath_robe: 2
   },
   LK: {
     king_pillow_case: 2,
     king_pillow_sham: 2,
     king_sheet: 2,
     king_duvet_cover: 1,
-    bath_towel: 1.5,
-    bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    bath_towel: 4,
+    bath_matt: 2,
+    face_cloth: 3,
+    hand_towel: 3,
+    bath_robe: 2
   },
   SuiteK: {
     king_pillow_case: 2,
     king_pillow_sham: 2,
     king_sheet: 2,
     king_duvet_cover: 1,
-    bath_towel: 1.5,
-    bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    bath_towel: 4,
+    bath_matt: 2,
+    face_cloth: 3,
+    hand_towel: 3,
+    bath_robe: 2
   }
 }
 
@@ -84,76 +84,76 @@ export const linens_for_stayOver = {
     queen_pillow_sham: 0,
     queen_sheet: 0,
     queen_duvet_cover: 0,
-    bath_towel: 2.8,
-    bath_matt: 2,
-    face_cloth: 2.8,
-    hand_towel: 2.3,
-    bath_robe: 1.9
+    bath_towel: 2,
+    bath_matt: 1,
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   SQQ: {
     queen_pillow_case: 0,
     queen_pillow_sham: 0,
     queen_sheet: 0,
     queen_duvet_cover: 0,
-    bath_towel: 2.8,
-    bath_matt: 2,
-    face_cloth: 2.8,
-    hand_towel: 2.3,
-    bath_robe: 1.9
+    bath_towel: 2,
+    bath_matt: 1,
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   SQ: {
     queen_pillow_case: 0,
     queen_pillow_sham: 0,
     queen_sheet: 0,
     queen_duvet_cover: 0,
-    bath_towel: 1.5,
+    bath_towel: 2,
     bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   SK: {
     king_pillow_case: 0,
     king_pillow_sham: 0,
     king_sheet: 0,
     king_duvet_cover: 0,
-    bath_towel: 1.5,
+    bath_towel: 2,
     bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    face_cloth: 3,
+    hand_towel: 2,
+    bath_robe: 2
   },
   DK: {
     king_pillow_case: 0,
     king_pillow_sham: 0,
     king_sheet: 0,
     king_duvet_cover: 0,
-    bath_towel: 1.5,
-    bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    bath_towel: 4,
+    bath_matt: 2,
+    face_cloth: 3,
+    hand_towel: 3,
+    bath_robe: 2
   },
   LK: {
     king_pillow_case: 0,
     king_pillow_sham: 0,
     king_sheet: 0,
     king_duvet_cover: 0,
-    bath_towel: 1.5,
-    bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    bath_towel: 4,
+    bath_matt: 2,
+    face_cloth: 3,
+    hand_towel: 3,
+    bath_robe: 2
   },
   SuiteK: {
     king_pillow_case: 0,
     king_pillow_sham: 0,
     king_sheet: 0,
     king_duvet_cover: 0,
-    bath_towel: 1.5,
-    bath_matt: 1,
-    face_cloth: 2.2,
-    hand_towel: 1.8,
-    bath_robe: 1.2
+    bath_towel: 4,
+    bath_matt: 2,
+    face_cloth: 3,
+    hand_towel: 3,
+    bath_robe: 2
   }
 }
